4c3c933879a0e50155b3e6f00e2dfa1c0f1152d2
pete <me@peterjlord.co.uk>
2019-07-01 16:14:54 +0100

    Buttons to switch colours

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -19,10 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(
-                constraint_layout, box_one_text, box_two_text, box_three_text,
-                        box_four_text, box_five_text
-            )
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout,
+                red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -39,6 +38,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             // Boxes using custom colors for background
             else -> view.setBackgroundColor(Color.LTGRAY)
